240bf7012bbf7424130822c44b952523c08d11c4
keongracun21 <erickpaskahbawoleh@gmail.com>
2025-08-10 23:46:47 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/style.css', 'resources/js/app.js'])
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
